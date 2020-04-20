1eb5071e1e94833ca233e2cc43c057b3ef5a99eb
Leo Dion <leogdion@brightdigit.com>
2020-04-20 13:09:26 -0400

    removed all other files

--- applications/mac/Application.swift
+++
@@ -1,4 +0,0 @@
-import Cocoa
-import SpeculidKit
-
-open class Application: SpeculidKit.Application {}
--- applications/xpc/main.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-let delegate = ServiceDelegate()
-let listener = NSXPCListener.service()
-listener.delegate = delegate
-listener.resume()
